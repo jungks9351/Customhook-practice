41c2d1cab4127558aa0f7638408d43e2d807803b
kimjungjin <jungks9351@gmail.com>
2022-10-16 19:21:03 +0900

    feat : Reference 라우팅 구현

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
 const Footer = () => {
-  return <FooterWrapper>@Copyright @2022 KimJungJin</FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <p>@Copyright @2022 KimJungJin</p>
+      <p>
+        Reference{' '}
+        <a href='https://usehooks.com' target='_blank' rel='noreferrer'>
+          https://usehooks.com
+        </a>
+      </p>
+    </FooterWrapper>
+  );
 };
 
 const FooterWrapper = styled.footer`
