6b925a60a8d92172da447352869c3145d76a477b
kimjungjin <jungks9351@gmail.com>
2022-10-16 19:21:29 +0900

    feat : useLocalStorage 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LocalStoragePage from './pages/LocalStoragePage';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/useLocalStorage' element={<LocalStoragePage />} />
         <Route path='/' element={<MainPage />} />
       </Routes>
     </BrowserRouter>
